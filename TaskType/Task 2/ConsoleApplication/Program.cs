58a9794bd59a1e4b7cf76aa75343e3e719eca712
LSokolovskaya <liubasokolovskaya@gmail.com>
2022-10-22 08:17:58 +0300

    Задачи Tasks (if,else)

--- TaskType/Task 2/ConsoleApplication/Program.cs
+++ TaskType/Task 2/ConsoleApplication/Program.cs
@@ -3,6 +3,7 @@ int p = 10;
 int m = 3;
 int result = p % m;
 Console.WriteLine($"Остаток от деления a на b : {result}");
+
 // Пользователь вводит с клавиатуры 2 числа: основание степени и показатель степени. Необходимо возвести число во введенную степень;
 Console.Write("Введите значение основания: ");
 var varible = Console.ReadLine();
@@ -11,14 +12,17 @@ Console.Write("Введите значение степени: ");
 varible = Console.ReadLine();
 int exponent = Convert.ToInt32(varible);
 Console.WriteLine($"Ответ:"+ Math.Pow(bounding,exponent));
+
 // Напишите программу, которая считывает длины двух катетов в прямоугольном треугольнике и выводит его площадь. Используйте формулу;
 Console.Write("Первый катет: ");
 int katet1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Второй катет: ");
 int katet2 = Convert.ToInt32(Console.ReadLine());
+
 // double square = 0.5*katet1*katet2;  
 double square = 0.5*katet1*katet2;
 Console.WriteLine($"Площадь треугольника равна:  {square}");
+
 // У известного американского писателя Рэя Бредбери есть роман «451 градус по
 // Фаренгейту». Напишите скрипт, который определяет, какой температуре по
 // шкале Цельсия соответствует указанное значение по шкале Фаренгейта.
@@ -26,6 +30,7 @@ Console.WriteLine($"Площадь треугольника равна:  {square
 int Fahrenheit = 451;
 int celsius = (Fahrenheit- 32) / 9 * 5;
 Console.WriteLine($"451°F = {celsius}°C");
+
 // Даны переменные a и b. Проверьте, что a делится без остатка на b. Если это так -
 // выведите 'Делится' и результат деления, иначе выведите 'Делится с остатком' и
 // само значение деления
@@ -37,6 +42,7 @@ int remainder = i%k;
 int remainder1 = i/k;
 if(remainder > 0){Console.WriteLine($"Делится с остатком {remainder}");}
 else{Console.WriteLine($"Делится {remainder1}");}
+
 // Вы вводите 2 числа a и b. Вам необходимо решить пример (2(а-b)-4(b-a))/2
 Console.Write("Введите число а: ");
 int l = Convert.ToInt32(Console.ReadLine());
@@ -44,6 +50,7 @@ Console.Write("Введите число b: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int answer = (2*(l-n)-4*(n-l))/2;
 Console.WriteLine($"Ответ: {answer}");
+
 // На вход программе подается число n – количество собачьих лет. Напишите
 // программу, которая вычисляет возраст собаки в человеческих годах.
 // В течение первых двух лет собачий год равен 10.5 человеческим годам. После
@@ -54,9 +61,11 @@ if (year<=2)
 {double age = year*10.5;
 Console.Write($"Если бы собака была человеком, то ей бы уже было {age} лет");}
 else {Console.WriteLine($"Если бы собака была человеком, то ей бы уже было {2*10.5+(n-2)*4} лет");};
+
 // Найдите квадратный корень из 245. Предварительно изучите System.Math и в
 // отдельности Math.Sqrt
 Console.WriteLine("√245 = " + Math.Sqrt(245));
+
 // Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число.
 // Предварительно изучите модуль Math и в отдельности Math.Min и Math.Max
 int a = 4;
@@ -70,6 +79,7 @@ int maximum = Math.Max(a,Math.Max(b,Math.Max(c,Math.Max(d,Math.Max(e,Math.Max(f,
 Console.WriteLine($"Самое большое число: {maximum}");
 int minimum = Math.Min(a,Math.Min(b,Math.Min(c,Math.Min(d,Math.Min(e,Math.Min(f,g)))))); 
 Console.WriteLine($"Самое маленькое число: {minimum}");
+
 // Напишите программу, вычисляющую объём куба и площадь его полной
 // поверхности, по введённому значению длины ребра. Используйте формулы:
 Console.Write("Длина ребра: ");
@@ -78,6 +88,7 @@ int sguare = 6*lenght*lenght;
 Console.WriteLine($"Площадь куба: {sguare}");
 int volume = lenght*lenght;
 Console.WriteLine($"Объем куба: {volume} ");
+
 // Напишите программу, в которой вычисляется сумма, разность и произведение
 // // двух целых чисел, введенных с клавиатуры.
 Console.WriteLine("Введите число a");
@@ -87,6 +98,7 @@ int second = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"сумма = {first+second}");
 Console.WriteLine($"Разность = {first-second}");
 Console.WriteLine($"Произведение = {first*second}");
+
 // Напишите программу для пересчёта величины временного интервала, заданного
 // в минутах, в величину, выраженную в часах и минутах:
 // Пример:
---
+++ TaskType/Task 3/ConsoleApplication/Program.cs
@@ -0,0 +1,90 @@
+﻿// 1. Вы вводите значение с клавиатуры. Если значение равно 0, то вывести ‘Ноль’,
+// если < 0, то вывести ‘Отрицательное’, если > 0, то вывести ‘Положительное’
+{
+    Console.WriteLine("Введите число: ");
+    int nomer = Convert.ToInt32(Console.ReadLine());
+    string communication;
+    if (nomer > 0)
+    {
+        communication = "Положительное";
+    }
+    else if (nomer < 0)
+    {
+        communication = "Отрицательно";
+    }
+    else
+    {
+        communication = "Ноль";
+    }
+    Console.WriteLine(communication);
+}
+
+// 2. Вы вводите значение с клавиатуры. Если значение четное, то вывести true, в
+// противном случае false
+{
+    Console.WriteLine("Введите число: ");
+    int nomer = Convert.ToInt32(Console.ReadLine());
+    string communication;
+    if (nomer % 2 == 0)
+    {
+        communication = "true";
+    }
+    else
+    {
+        communication = "false";
+    }
+    Console.WriteLine(communication);
+}
+
+// 3. Пользователь вводит с клавиатуры число. Найдите корень введенного числа
+// (если же результат – число с плавающей точкой, то округлить до 2 знаков после
+// запятой).
+{
+    Console.WriteLine("Введите число: ");
+    double nomer = Convert.ToDouble(Console.ReadLine());
+    double answer = Math.Round(Math.Sqrt(nomer), 2);
+    Console.WriteLine(answer);
+}
+
+// 4. Пользователь вводит с клавиатуры 2 числа: a и b. Проверьте, что a делится без
+// остатка на b. Если это так - выведите 'Делится' и результат деления, иначе
+// выведите 'Делится с остатком' и остаток от деления
+{
+    Console.WriteLine("Введите первое число: ");
+    int nomer1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    int nomer2 = Convert.ToInt32(Console.ReadLine());
+    int answer;
+    if (nomer1 % nomer2 == 0)
+    {
+        answer = nomer1 / nomer2;
+        Console.WriteLine("Делитcя");
+    }
+    else
+    {
+        answer = nomer1 % nomer2;
+        Console.WriteLine($"Делитcя с остатком: {answer}");
+    }
+}
+
+// 5. Пользователь вводит с клавиатуры число. Найдите корень введенного числа
+// (если же результат – число с плавающей точкой, то округлить то целых)
+{
+    Console.WriteLine("Введите число: ");
+    int nomer = Convert.ToInt32(Console.ReadLine());
+    double answer = Math.Round(Math.Sqrt(nomer), 0);
+    Console.WriteLine(answer);
+}
+// 6. Даны три числа a, b, c. Напишите программу, которая находит корни квадратного
+// уравнения
+// ax
+// 2 + bx + c = 0
+// Если уравнение имеет два корня, то следует вывести их в порядке возрастания.
+// 
+    // Console.WriteLine("Введите первое число: ");
+    // int nomer1 = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите второе число: ");
+    // int nomer2 = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите третье число: ");
+    // int nomer3 = Convert.ToInt32(Console.ReadLine());
+   
