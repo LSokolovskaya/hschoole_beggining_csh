893fcb6a40efa38284987008eede8f7a5fc3b478
LSokolovskaya <liubasokolovskaya@gmail.com>
2022-11-13 21:27:47 +0300

    Almost ready

--- TaskType/Task5/ConsoleApplication/Program.cs
+++ TaskType/Task5/ConsoleApplication/Program.cs
@@ -115,6 +115,7 @@
 // • Вырежите из нее 2 и 3 слова.
 // • Найдите индекс 2 подстроки
 
+
 // 11. Пользователь вводит дату в формате ‘х-х-х'. Замените все дефисы на '!'
 {
     Console.WriteLine("Введите дату x-x-x");
@@ -147,7 +148,7 @@
 // 13. Дан массив ['я', 'учу', 'javascript', '!']. С помощью метода join преобразуйте массив в
 // строку 'я+учу+javascript+!'.
 {
-    string[] array = new[] {"я", "учу", "javascript", "!"};
+    string[] array = new[] { "я", "учу", "javascript", "!" };
     System.Console.WriteLine(string.Join("+", array));
 }
 
@@ -186,12 +187,83 @@
     }
 }
 // 16. Пользователь вводит строку. Необходимо посчитать количество гласных (for)
+{
+    System.Console.WriteLine("Введите строку");
+    string? text = Console.ReadLine();
+    if (!string.IsNullOrEmpty(text))
+    {
+        text = text.ToLower();
+        string letters = "eyuioa";
+        int result = 0;
+        char[] array = text.ToCharArray();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (letters.Contains(array[i]))
+            {
+                result += 1;
+            }
+        }
+        System.Console.WriteLine(result);
+    }
+    else
+    {
+        System.Console.WriteLine("Ошибка ввода");
+    }
+}
 
 // 17. Пользователь вводит строку. Необходимо посчитать количество согласных (for)
+{
+    System.Console.WriteLine("Введите строку");
+    string? text = Console.ReadLine();
+    if (!string.IsNullOrEmpty(text))
+    {
+        text = text.ToLower();
+        string letters = "qwrtpsdfghjklzxcvbnm";
+        int result = 0;
+        char[] array = text.ToCharArray();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (letters.Contains(array[i]))
+            {
+                result += 1;
+            }
+
+        }
+        System.Console.WriteLine(result);
+    }
+    else
+    {
+        System.Console.WriteLine("Ошибка ввода");
+    }
+}
 
 // 18. Пользователь вводит строку. Необходимо вывести все гласные отдельной
 // строкой (for)
 // fullstack => ua
+{
+    System.Console.WriteLine("Введите строку");
+    string? text = Console.ReadLine();
+    if (!string.IsNullOrEmpty(text))
+    {
+        text = text.ToLower();
+        string letters = "eyuioa";
+        string result = "";
+        char[] array = text.ToCharArray();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (letters.Contains(array[i]))
+            {
+                result += array[i];
+            }
+
+        }
+        System.Console.WriteLine(result);
+    }
+    else
+    {
+        System.Console.WriteLine("Ошибка ввода");
+    }
+}
 
 // 19. На вход программе подается строка. Преобразуйте первую букву каждого слова
 // строки в верхний регистр (for)
@@ -201,11 +273,18 @@
     for (int i = 0; i < array.Length; i++)
     {
         string result = array[i];
-        result = Convert.ToString(char.ToUpper(result[0]));
-        result += result.Remove(0);
-        System.Console.Write(result);
+        string resultToUpper = Convert.ToString(char.ToUpper(result[0]));
+        resultToUpper += result.Remove(0, 1);
+        System.Console.Write(resultToUpper);
     }
 }
+
+//Пример из презентации
+// {
+//     string text = "fgh ghj";
+//     System.Console.WriteLine(text.Remove(0, 1));
+// }
+
 // int number = text?.GetHashCode() ?? int.MaxValue;
 // //==  true
 // int number = text != null ? text.GetHashCode() : int.MaxValue;
@@ -213,6 +292,17 @@
 // 20.На вход программе подается строка, где каждое слово заканчивается “_”.
 // Программа должна убрать “_” и первую букву каждого слова (помимо первого),
 // преобразовать в верхний регистр (for)
+{
+    string? text = Console.ReadLine();
+    string[] array = text?.Split("_") ?? new string[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        string result = array[i];
+        string resultToUpper = Convert.ToString(char.ToUpper(result[0]));
+        resultToUpper += result.Remove(0, 1);
+        System.Console.Write(resultToUpper);
+    }
+}
 
 // 21. На вход программе подается строка, состоящая из имени и фамилии человека,
 // разделенных одним пробелом. Напишите программу, которая проверяет, что имя
@@ -220,31 +310,224 @@
 // Chris Alan => true
 // chris alan => false
 
+
 // 22. На вход программе подается строка. Напишите программу, которая меняет
 // регистр символов, другими словами замените все строчные символы заглавными
 // и наоборот.
 // Swap Case => sWAP cASE
+{
+    string? text = Console.ReadLine();
+    text = text?.Trim();
+    if (!string.IsNullOrEmpty(text))
+    {
+        string result = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLower(text[i]))
+            {
+                result += char.ToUpper(text[i]).ToString();
+            }
+            else if (char.IsUpper(text[i]))
+            {
+                result += char.ToLower(text[i]).ToString();
+            }
+        }
+
+        System.Console.WriteLine(result);
+    }
+    else
+    {
+        System.Console.WriteLine("Ошибка ввода");
+    }
+}
+
 
 // 23. На вход программе подается строка текста. Напишите программу, которая
 // определяет является ли строка хорошей. Текст хороший, если содержит
 // подстроку «хорош» во всевозможных регистрах.
 // я очень хороший текст => true
+{
+    System.Console.WriteLine("Введите строку");
+    string? text = Console.ReadLine();
+    text = text?.ToLower();
+    string? result = Convert.ToString(text?.Contains("хорош"));
+    Console.WriteLine($"{text} => {result}");
+}
+
+// Пример
+// {
+//     string text = "123456";
+//     System.Console.WriteLine(text.IndexOf("8"));
+// }
+
+// Пример
+//     string text = "123456";
+// System.Console.WriteLine(text.Contains("8"));
 
 // 24.На вход программе подается строка. Напишите программу, которая подсчитывает
 // количество буквенных символов в нижнем регистре.
 // abcABCD12345 => 3
+{
+    string? text = Console.ReadLine();
+    text = text?.Trim();
+    if (!string.IsNullOrEmpty(text))
+    {
+        string result = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLower(text[i]))
+            {
+                int number = 0;
+                number += 1;
+                result += text[i];
+            }
+        }
+        System.Console.WriteLine(result.Length);
+    }
+    else
+    {
+        System.Console.WriteLine("Ошибка ввода");
+    }
+}
+
 
 // 25. На вход программе подается строка из букв А, Г, Ц, Т. Напишите программу,
 // которая подсчитывает сколько букв
 // АааГГЦЦцТТтттА => А – 4 Г – 2 Ц – 3 Т – 5
 
+{
+    string? text = Console.ReadLine();
+    text = text?.Trim();
+    text = text?.ToUpper();
+    string result1 = "";
+    string result2 = "";
+    string result3 = "";
+    string result4 = "";
+    if (!string.IsNullOrEmpty(text))
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == 'А')
+            {
+                int number = 0;
+                number += 1;
+                result1 += text[i];
+            }
+            else if (text[i] == 'Г')
+            {
+                int number = 0;
+                number += 1;
+                result2 += text[i];
+            }
+            else if (text[i] == 'Ц')
+            {
+                int number = 0;
+                number += 1;
+                result3 += text[i];
+            }
+            else if (text[i] == 'Т')
+            {
+                int number = 0;
+                number += 1;
+                result4 += text[i];
+            }
+        }
+        System.Console.WriteLine($"{text} => A - {result1.Length}  Г - {result2.Length}  Ц - {result3.Length}  Т - {result4.Length}");
+    }
+    else
+    {
+        System.Console.WriteLine("null");
+    }
+}
+
+{
+    System.Console.WriteLine("Введите строку");
+    string? text = Convert.ToString(Console.ReadLine());
+    text = text?.ToLower();
+    int result1 = 0;
+    int result2 = 0;
+    int result3 = 0;
+    int result4 = 0;
+    if (!string.IsNullOrEmpty(text))
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == 'а')
+            {
+                result1 += 1;
+            }
+            if (text[i] == 'г')
+            {
+                result2 += 1;
+            }
+            if (text[i] == 'ц')
+            {
+                result3 += 1;
+            }
+            if (text[i] == 'т')
+            {
+                result4 += 1;
+            }
+        }
+        System.Console.WriteLine($"{text} => A - {result1}  Г - {result2}  Ц - {result3}  Т - {result4}");
+    }
+    else
+    {
+        System.Console.WriteLine("null");
+    }
+}
+
+
 // 26. На вход программе подается строка текста. Напишите программу, которая
 // проверяет, что строка заканчивается подстрокой .com или .ru. Если
 // заканчивается, то вывести true, в противном случае false
+{
+    string? text = Console.ReadLine();
+    if (text.EndsWith(".com"))
+    {
+        System.Console.WriteLine(text.EndsWith(".com"));
+    }
+    else if (text.EndsWith(".ru"))
+    {
+        System.Console.WriteLine(text.EndsWith(".ru"));
+    }
+    else
+    {
+        System.Console.WriteLine("Ошибка ввода");
+    }
+}
 
 // 27. На вход подается url. Если он начинается с http, содержит хотя бы один “/” и
-// заканчивается
-
+// заканчивается подстрокой .com или .ru, то вывести true, в противном случае false
+{
+    string? text = Console.ReadLine();
+    if (text.StartsWith("http"))
+    {
+        if (Convert.ToBoolean(text.IndexOf("/") > 0))
+        {
+            if (text.EndsWith(".com"))
+            {
+                System.Console.WriteLine(text.EndsWith(".com"));
+            }
+            else if (text.EndsWith(".ru"))
+            {
+                System.Console.WriteLine(text.EndsWith(".ru"));
+            }
+            else
+            {
+                System.Console.WriteLine("Ошибка ввода");
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("false");
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("false");
+    }
+}
 
 
 
@@ -263,109 +546,4 @@
 //  char sym = 'a';
 // if ('a' == sym) {}
 
-
-// 1.У тебя есть текст из нескольких преложений (преложения разделены символом .), который содержит символы 
-// в разном регистре, преобразовать текст в правильный вид к примеру: " fsadf fRFFsadf FR fdwgt. rftDFRE# frfew. fdsw .rff " 
-// =>"Fsadf frffsadf fr fdwgt. Rftdfre# frfew. Fdsw. Rff."
-{
-    string? text = " fsadf fRFFsadf FR fdwgt. rftDFRE# frfew. fdsw. rff ";
-    text = Convert.ToString(text.Split("."));
-    System.Console.WriteLine(text);
-}
-
-
-
-
-// // 3 ввести два числа если четные то вывести сумму, если нечетные разделить первое на второе 
-// // (результат вывести с точка) если 0 то не можем провести операцию
-{
-    double number1 = Convert.ToInt32(Console.ReadLine());
-    double number2 = Convert.ToInt32(Console.ReadLine());
-    if (number2 > 0)
-    {
-        if (number1 % 2 == 0 && number2 % 2 == 0)
-        {
-            double result = number1 + number2;
-            System.Console.WriteLine(result);
-        }
-        else if (number1 % 2 != 0 && number2 % 2 != 0)
-        {
-            double result = number1 / number2;
-            System.Console.WriteLine(result);
-        }
-    }
-    else
-    {
-        System.Console.WriteLine("нельзя вывести операцию");
-    }
-}
-
-
-// // Найти факториал числа
-//for
-{
-    string? consoleText = Console.ReadLine();
-    if (int.TryParse(consoleText, out int number) && number > 0)
-    {
-        int result = 1;
-        for (int i = 1; i <= number; i++)
-        {
-            result *= i;
-        }
-        Console.WriteLine(result);
-    }
-    else
-    {
-        Console.WriteLine("Вы ввели не натуральное число");
-    }
-}
-//while
-{
-    string? consoleText = Console.ReadLine();
-    if (int.TryParse(consoleText, out int number) && number > 0)
-    {
-        int result = 1;
-        int i = 1;
-        while (i <= number)
-        {
-            result *= i;
-            i++;
-        }
-        Console.WriteLine(result);
-    }
-    else
-    {
-        Console.WriteLine("Вы ввели не натуральное число");
-    }
-}
-
-// string text = "dfghj";
-// int number1 = Convert.ToInt32(text);
-// int number2 = int.Parse(text);
-// int number3 = int.TryParse(text);
-// bool result = int.TryParse(text, out int number);
-// char index = '1';
-// text = Convert.ToString(index);
-// text = index.ToString();
-
-
-// с клавиатуры вводится строка, которая содержит разные символы, сложить все цифры и вывести результат
-{
-    string? text = Console.ReadLine();
-    int result = 0;
-    for (int i = 0; i < text.Length; i++)
-    {
-        if (int.TryParse(Convert.ToString(text[i]), out int number))
-        {
-            result += number;
-            // text = Convert.ToString(text.Split(""));
-            // i += Convert.ToInt32(text[i]);
-        }
-    }
-    System.Console.WriteLine(result);
-    //     {
-    //         string text = "d,fg,hj";
-    //         System.Console.WriteLine(text.Split(",").Length);
-    //     }
-}
 
